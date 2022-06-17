d484fa7ba7bf6728c13b87453333afd6fda51c9d
Rotem Cohen <rotem.cohen1996@gmail.com>
2022-06-17 14:25:10 +0300

    autocomplete design

--- shared/interfaces/item.interface.ts
+++ shared/interfaces/item.interface.ts
@@ -1,5 +1,11 @@
 export interface Item {
     sku: number;
     name: string;
+    type: string;
     price: number;
+    upc: string;
+    category: {id: string; name: string}[];
+    shipping: number;
+    description: string;
+    image: string;
 }
