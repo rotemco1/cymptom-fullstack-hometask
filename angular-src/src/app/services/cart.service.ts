df8a5d595afd57c5149012d05e68b5ad4a9b33f4
Rotem Cohen <rotem.cohen1996@gmail.com>
2022-06-18 12:55:14 +0300

    add total price

--- angular-src/src/app/services/cart.service.ts
+++ angular-src/src/app/services/cart.service.ts
@@ -4,6 +4,7 @@ import { Item } from "../../../../shared/interfaces";
 @Injectable()
 export class CartService {
     private _cart: Item[] = [];
+    private _totalPrice = 0;
 
     constructor() { }
 
@@ -11,11 +12,17 @@ export class CartService {
         return this._cart;
     }
 
+    get totalPrice() {
+        return this._totalPrice.toFixed(2);
+    }
+
     addToCart(item: Item) {
         this._cart.push(item);
+        this._totalPrice += item.price;
     }
 
     removeFromCart(item: Item) {
-        this._cart = this._cart.filter(i => i !== item)
+        this._cart = this._cart.filter(i => i !== item);
+        this._totalPrice -= item.price;
     }
 }
